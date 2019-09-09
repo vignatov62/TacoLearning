0da2c2e1d3b84d388f9fbc661c49225cee7e81d2
Basil <vignatov62@yandex.ru>
2019-09-09 20:03:59 +0300

    3.1 Jdbc Repositories

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,6 +1,8 @@
 package tacos;
 
 import lombok.Data;
+
+import java.util.Date;
 import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -8,9 +10,14 @@ import javax.validation.constraints.Size;
 @Data
 public class Taco {
 
+    private Long id;
+
+    private Date createdAt;
+
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+
     @Size(min=1, message="You must choose at least 1 ingredient")
     private List<String> ingredients;
 
